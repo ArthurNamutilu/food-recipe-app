ed3bb7cf3c8462c43d794d92762030a2ef595494
ArthurNamutilu <arthurggnamutilu@gmail.com>
2024-03-05 23:24:35 +0300

    -

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/navbar";
+import Home from "./pages/home";
+import Favorites from "./pages/favorites";
+import Details from "./pages/details";
+
+function App() {
+  return (
+    <div>
+      <div className="min-h-screen p-6 bg-white text-gray-600 text-lg">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/recipe-item/:id" element={<Details />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
