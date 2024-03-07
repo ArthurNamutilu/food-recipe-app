166843e9824438d199310c30271e73ab414843c8
ArthurNamutilu <arthurggnamutilu@gmail.com>
2024-03-08 00:52:51 +0300

    added adding/removing to favorite button

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,9 +1,17 @@
 import { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const GlobalContext = createContext(null);
 
 const GlobalState = ({ children }) => {
   const [searchParam, setSearchParam] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [recipeList, setRecipeList] = useState([]);
+  const [recipeDetailsData, setRecipeDetailsData] = useState(null);
+  const [favoritesList, setFavoritesList] = useState([]);
+
+  const navigate = useNavigate();
+
   async function handleSubmit(event) {
     event.preventDefault();
     try {
@@ -11,15 +19,53 @@ const GlobalState = ({ children }) => {
         `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`
       );
       const data = await res.json();
-      console.log(data);
+      if (data?.data?.recipes) {
+        setRecipeList(data?.data?.recipes);
+        setLoading(false);
+        setSearchParam("");
+        navigate("/");
+      }
     } catch (e) {
+      setLoading(false);
+      setSearchParam("");
       console.log(e);
     }
   }
 
+  function handleAddToFavorite(getCurrentItem) {
+    console.log(getCurrentItem);
+    let cpyFavoritesList = [...favoritesList];
+    const index = cpyFavoritesList.findIndex(
+      (item) => item.id === getCurrentItem.id
+    );
+
+    if (index === -1) {
+      cpyFavoritesList.push(getCurrentItem);
+    } else {
+      cpyFavoritesList.splice(index);
+    }
+
+    setFavoritesList(cpyFavoritesList);
+    console.log(cpyFavoritesList, "Fav list");
+  }
+
+  // console.log(cpyFavoritesList, "Fav list");
+
   return (
     <GlobalContext.Provider
-      value={{ searchParam, setSearchParam, handleSubmit }}
+      value={{
+        searchParam,
+        setSearchParam,
+        handleSubmit,
+        loading,
+        recipeList,
+        recipeDetailsData,
+        setRecipeDetailsData,
+        favoritesList,
+        setFavoritesList,
+        handleAddToFavorite,
+        favoritesList,
+      }}
     >
       {children}
     </GlobalContext.Provider>
